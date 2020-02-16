ccfdb5fe4acbe5bb4fb6d13a4157c54d5b636ce0
Hayk Sargsyan <hayk.gukasyan@bk.ru>
2020-02-16 11:16:51 +0400

    add validation

--- client/src/components/todoItem.tsx
+++ client/src/components/todoItem.tsx
@@ -1,73 +1,111 @@
-import React from "react";
+import React, { ChangeEvent } from "react";
 import "../App.css";
 import "materialize-css";
 import { Link } from "react-router-dom";
 interface Icolection {
-    _id: number;
-    title: string;
-    condidate: boolean;
+  _id: number;
+  title: string;
+  condidate: boolean;
 }
 
 const Home = () => {
-    const [colection, setColection] = React.useState<Icolection[]>([]);
-    const [check, setCheck] = React.useState<boolean>(false);
-    React.useEffect(() => {
-        fetch("http://localhost:8000/")
-            .then(response => response.json())
-            .then(data => setColection(data));
-    }, []);
+  const [colection, setColection] = React.useState<Icolection[]>([]);
+  const [check, setCheck] = React.useState<boolean>(false);
+  const [create, setCreate] = React.useState<string>("");
+  React.useEffect(() => {
+    fetch("http://localhost:8000/")
+      .then(response => response.json())
+      .then(data => setColection(data));
+  }, []);
 
-    async function sendHandler(id: number) {
-        await fetch("http://localhost:8000/", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(colection[id])
-        });
+  async function sendHandler(id: number) {
+    await fetch("http://localhost:8000/", {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(colection[id])
+    });
+  }
+  const createHandlerPost = async (): Promise<void> => {
+    await fetch("http://localhost:8000/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        title: create,
+        condidate: false
+      })
+    });
+    setCreate("");
+  };
+
+  const changeHandler = (index: number) => {
+    const clone = colection;
+    setCheck(!check);
+    if (clone[index].condidate) {
+      clone[index].condidate = false;
+      return setColection(clone);
     }
-
-    const changeHandler = (index: number) => {
-        const clone = colection;
-        setCheck(!check);
-        if (clone[index].condidate) {
-            clone[index].condidate = false;
-            return setColection(clone);
-        }
-        clone[index].condidate = true;
-        setColection(clone);
-    };
-    console.log("hi");
-    return (
-        <div className="container">
-            <div className="todo-container">
-                {colection.map((item: Icolection, index: number) => (
-                    <p key={item._id}>
-                        <label>
-                            {item.condidate === true ? (
-                                <input
-                                    onChange={() => changeHandler(index)}
-                                    type="checkbox"
-                                    checked={true}
-                                />
-                            ) : (
-                                    <input
-                                        onChange={() => changeHandler(index)}
-                                        type="checkbox"
-                                        checked={false}
-                                    />
-                                )}
-                            <span className={item.condidate ? "done" : ""} >{item.title}</span>
-                            <input type="hidden" value={item._id} />
-                        </label>
-                        <div>
-                            <button className="btn red  edit-btn"><Link to={`/${item._id}`}>Edit</Link></button>
-                            <button onClick={() => sendHandler(index)} className="btn primry">Save</button>
-                        </div>
-                    </p>
-                ))}
-            </div>
+    clone[index].condidate = true;
+    setColection(clone);
+  };
+  const createHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    setCreate(event.target.value);
+  };
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="input-field col m8">
+          <input
+            value={create}
+            id="first_name2"
+            type="text"
+            className="validate"
+            onChange={createHandler}
+          />
+          <label className="active">Create ToDo</label>
+          <button
+            onClick={createHandlerPost}
+            className="btn waves-effect waves-light"
+          >
+            Create
+          </button>
         </div>
-    );
+      </div>
+      <div className="todo-container">
+        {colection.map((item: Icolection, index: number) => (
+          <p key={item._id}>
+            <label>
+              {item.condidate === true ? (
+                <input
+                  onChange={() => changeHandler(index)}
+                  type="checkbox"
+                  checked={true}
+                />
+              ) : (
+                <input
+                  onChange={() => changeHandler(index)}
+                  type="checkbox"
+                  checked={false}
+                />
+              )}
+              <span className={item.condidate ? "done" : ""}>{item.title}</span>
+              <input type="hidden" value={item._id} />
+            </label>
+            <div>
+              <button className="btn red  edit-btn">
+                <Link to={`/${item._id}`}>Edit</Link>
+              </button>
+              <button onClick={() => sendHandler(index)} className="btn primry">
+                Save
+              </button>
+            </div>
+          </p>
+        ))}
+      </div>
+    </div>
+  );
 };
 export default Home;
--- routes/home.ts
+++ routes/home.ts
@@ -1,15 +1,39 @@
 import { Router, Request, Response } from "express";
-import todo, { IToDo} from "../modules/todo";
+import todo, { IToDo } from "../modules/todo";
+import Joi from "joi";
 const router = Router();
 
 router.get("/", async (req: Request, res: Response) => {
   const colection = await todo.find({});
   return res.json(colection);
 });
-router.post("/", async (req: Request, res: Response) => {
+router.put("/", async (req: Request, res: Response) => {
   const colection: IToDo = await todo.findById(req.body._id);
   colection.condidate = !!req.body.condidate;
   await colection.save();
+});
+router.post("/", async (req: Request, res: Response) => {
+  console.log("in");
+  const schema = Joi.object().keys({
+    title: Joi.string()
+      .min(2)
+      .max(30)
+      .required(),
+    condidate: Joi.boolean()
+  });
+  Joi.validate(
+    req.body,
+    schema,
+    async (err: Joi.ValidationError, result: any) => {
+      if (err) {
+        return res.status(400).json({
+          message: "somthing is wrong"
+        });
+      }
+      const colection: IToDo = await todo.create(req.body);
+      return colection;
+    }
+  );
 });
 router.get("/:id", async (req, res) => {
   const toDo = await todo.findById(req.params.id);
